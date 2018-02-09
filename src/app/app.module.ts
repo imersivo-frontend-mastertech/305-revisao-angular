5fccdefcaea7d079361bda2f1a4f7aac35fada04
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-09 21:29:08 -0200

    Finaliza os componentes iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopoComponent } from './topo/topo.component';
+import { BannerComponent } from './banner/banner.component';
+import { TextoComponent } from './texto/texto.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopoComponent,
+    BannerComponent,
+    TextoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
