a01d8bb1f5bf33843e3b2d63e08008c97ce9d39a
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-09 21:47:54 -0200

    Programa a sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  sidebarOculta = true;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  alternarSidebar(){
+    // if(this.sidebarOculta){
+    //   this.sidebarOculta = false;
+    // }else{
+    //   this.sidebarOculta = true;
+    // }
+    this.sidebarOculta = !this.sidebarOculta;
+    
   }
 
 }
