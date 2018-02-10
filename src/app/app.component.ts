5e688d890bfc8cc14db3b76db7b547a9e470559b
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-09 22:15:18 -0200

    Programa os textos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  itens = [
+    {
+      "titulo": "Texto 1",
+      "texto": "Lorem ipsum dolor sit amet, etiam dicam corrumpit ex vis, ipsum vidisse te mel. Nec dicta legere at. Senserit scribentur per te, est vidit facilisis ut, id justo molestiae ius. Pro iuvaret cotidieque no, et sonet comprehensam eam. Ridens scriptorem id sea, quo noster aperiam intellegam ne. Has no tale vocent, ei duo illud delicata, ea mei erant delicatissimi."
+    },
+    {
+      "titulo": "Texto 2",
+      "texto": "In mei similique percipitur. Animal disputando sea eu, te sale latine euripidis eum, cu dolores molestiae voluptatibus est. Illum ludus lobortis at qui. Quo at altera omittam periculis, minim volutpat qui ad."
+    },
+    {
+  
+      "titulo": "Texto 3",
+      "texto": "Impetus diceret eu nam. Ad duo elit nullam omittam. Eos cu erroribus definitionem, solum timeam nam an. Ad his feugiat vituperata efficiendi. No cum dicant delenit vocibus, id pri soleat numquam, populo euismod definiebas vis ea. Ex dicit delenit vel, mei feugiat convenire incorrupte at."
+    }
+  ];
 }
--- src/app/texto/texto.component.ts
+++ src/app/texto/texto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-texto',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./texto.component.scss']
 })
 export class TextoComponent implements OnInit {
+  @Input() titulo;
+  @Input() texto;
 
   constructor() { }
 
